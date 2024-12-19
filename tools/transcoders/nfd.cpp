70c5af5bcc4c026b83f94dd7516d462aacd943a5
Rob Cameron <cameron@cs.sfu.ca>
2024-12-18 20:47:03 -0800

    Layout additional pipeline steps for completion of canonical reordering

--- tools/transcoders/nfd.cpp
+++ tools/transcoders/nfd.cpp
@@ -621,7 +621,20 @@ XfrmFunctionType generate_pipeline(CPUDriver & driver) {
     P.CreateKernelCall<P2SKernel>(Violation_CCC_Basis, ViolationCCCBytes);
     SHOW_BYTES(ViolationCCCBytes);
 
-    //P.CreateKernelCall<StdOutKernel>(OutputBytes);
+/*  TO DO */
+    StreamSet * ReorderedBytes = P.CreateStreamSet(1, 8);
+//  P.CreateKernelCall<CanonicalReordering>(ViolationSeqBytes, ViolationCCCBytes, ReorderedBytes);
+
+    StreamSet * ReorderedOutput = P.CreateStreamSet(1, 8);
+//  SpreadByMask(P, U8_ViolationSeq, ReorderedBytes, ReorderedStream);
+
+    StreamSet * OutputBytes = P.CreateStreamSet(1, 8);
+    P.CreateKernelCall<P2SKernel>(Output_Basis, OutputBytes);
+
+    StreamSet * ReorderedOutput = P.CreateStreamSet(1, 8);
+//  P.CreateKernelCall<StreamsMerge>(OutputBytes, ReorderedBytes, ReorderedOutput);
+
+//  P.CreateKernelCall<StdOutKernel>(ReorderedOutput);
 
     return P.compile();
 }
