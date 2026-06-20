ad64d7362df3362b085a4ee1493d9910e007ffc5
Rob Cameron <cameron@cs.sfu.ca>
2026-06-20 15:33:17 -0700

    Permutation supporting fixed length terms

--- lib/re/transforms/expand_permutes.cpp
+++ lib/re/transforms/expand_permutes.cpp
@@ -1,8 +1,10 @@
 #include <re/adt/adt.h>
+#include <re/analysis/re_analysis.h>
 #include <re/transforms/re_transformer.h>
 #include <re/transforms/expand_permutes.h>
 #include <re/printer/re_printer.h>
 #include <llvm/Support/raw_ostream.h>
+#include <llvm/Support/ErrorHandling.h>
 
 using namespace llvm;
 
@@ -12,29 +14,44 @@ namespace re {
 
 class ExpandPermutes final : public RE_Transformer {
 public:
-    ExpandPermutes() : RE_Transformer("ExpandPermutes") {}
+    ExpandPermutes(const cc::Alphabet * lengthAlpha) : 
+        RE_Transformer("ExpandPermutes"), mLengthAlphabet(lengthAlpha) {}
     RE * transformPermute(Permute * p) override;
+private:
+    const cc::Alphabet * mLengthAlphabet;
 };
 
 RE * ExpandPermutes::transformPermute(Permute * p) {
     unsigned perm_size = p->size();
-    std::vector<RE *> alts;
+    unsigned total_length = 0;
+    std::vector<RE *> alts(perm_size);
+    std::vector<unsigned> alt_lgth(perm_size);
+    unsigned i = 0;
     for (auto perm : *p) {
-        alts.push_back(perm);
+        alts[i] = perm;
+        auto rg =  getLengthRange(perm, mLengthAlphabet);
+        if (rg.first != rg.second) {
+            llvm::report_fatal_error("Variable length permutation terms are prohibited.");
+        }
+        alt_lgth[i] = rg.first;
+        total_length += rg.first;
+        i++;
     }
-    std::vector<RE *> elems;
+    std::vector<RE *> elems(perm_size + 1);
     RE * anyAlt = makeAlt(alts.begin(), alts.end());
     elems.push_back(makeRep(anyAlt, perm_size, perm_size));
+    i = 1;
     for (auto perm : *p) {
-        RE * negated = makeDiff(anyAlt, perm);
-        RE * r = makeSeq({perm, makeRep(negated, 0, perm_size - 1)});
-        elems.push_back(makeLookBehindAssertion(r));
+        unsigned remlgth = total_length - alt_lgth[i];
+        RE * r = makeSeq({perm, makeRep(makeAny(mLengthAlphabet), 0, remlgth)});
+        elems[i] = makeLookBehindAssertion(r);
+        i++;
     }
     return makeSeq(elems.begin(), elems.end());
 }
 
-RE * expandPermutes(RE * r) {
-    return ExpandPermutes().transformRE(r);
+RE * expandPermutes(RE * r, const cc::Alphabet * lengthAlpha) {
+    return ExpandPermutes(lengthAlpha).transformRE(r);
 }
 
 }
