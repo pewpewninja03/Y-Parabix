ecee1ff3c0aa7432b844e8fecb17c0afe24b0ec0
Tommy Duong <156570294+pewpewninja03@users.noreply.github.com>
2026-06-23 03:13:17 -0700

    Fixed format

--- include/util/not_null.h
+++ include/util/not_null.h
@@ -2,24 +2,25 @@
 
 #include <cassert>
 
-template <typename T> struct not_null {
-  not_null(T const value) : _value(value) { assert(_value); }
-  not_null(std::nullptr_t) = delete;
-  not_null(unsigned) = delete;
-  operator T() const { return _value; }
-  T operator->() const { return _value; }
-  T get() const { return _value; }
-
+template<typename T>
+struct not_null {
+    not_null(T const value) : _value(value) { assert(_value); }
+    not_null(std::nullptr_t) = delete;
+    not_null(unsigned) = delete;
+    operator T() const { return _value; }
+    T operator-> () const { return _value; }
+    T get() const { return _value; }
 private:
-  T const _value;
+    T const  _value;
 };
 
-template <typename T> struct no_conversion {
-  no_conversion(T const value) : _value(value) {}
-  template <typename U> no_conversion(U) = delete;
-  operator T() const { return _value; }
-  T get() const { return _value; }
-
+template<typename T>
+struct no_conversion {
+    no_conversion(T const value) : _value(value) { }
+    template <typename U> no_conversion(U) = delete;
+    operator T() const { return _value; }
+    T get() const { return _value; }
 private:
-  T const _value;
+    T const  _value;
 };
+
