01a6d679b3e73fe8eb5c80d54de56533226e5598
pewpewninja03 <tommyduong03@gmail.com>
2026-06-23 03:10:45 -0700

    Fixed compilation error by including cassert header

--- include/util/not_null.h
+++ include/util/not_null.h
@@ -1,25 +1,25 @@
 #pragma once
 
-template<typename T>
-struct not_null {
-    not_null(T const value) : _value(value) { assert(_value); }
-    not_null(std::nullptr_t) = delete;
-    not_null(unsigned) = delete;
-    operator T() const { return _value; }
-    T operator-> () const { return _value; }
-    T get() const { return _value; }
+#include <cassert>
+
+template <typename T> struct not_null {
+  not_null(T const value) : _value(value) { assert(_value); }
+  not_null(std::nullptr_t) = delete;
+  not_null(unsigned) = delete;
+  operator T() const { return _value; }
+  T operator->() const { return _value; }
+  T get() const { return _value; }
+
 private:
-    T const  _value;
+  T const _value;
 };
 
-template<typename T>
-struct no_conversion {
-    no_conversion(T const value) : _value(value) { }
-    template <typename U> no_conversion(U) = delete;
-    operator T() const { return _value; }
-    T get() const { return _value; }
+template <typename T> struct no_conversion {
+  no_conversion(T const value) : _value(value) {}
+  template <typename U> no_conversion(U) = delete;
+  operator T() const { return _value; }
+  T get() const { return _value; }
+
 private:
-    T const  _value;
+  T const _value;
 };
-
-
